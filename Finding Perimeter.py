dcd917e6c1eb440a9b4db25fc61dc310e7bda0c6
Abhinav-cloud482 <a74835655@gmail.com>
2025-08-05 14:29:54 +0530

    Add files via upload

---
+++ Finding Perimeter.py
@@ -0,0 +1,7 @@
+# perimeter of a rectangle
+
+a = float(input(" Length of side a: "))  #first length 
+b = float(input(" Length of side b: "))  #second legth
+perimeter = 2 * (a+b)  #perimeter
+
+print(perimeter) #printing that perimeter
